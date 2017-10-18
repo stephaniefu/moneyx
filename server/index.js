bbe0faa2442b5fc853d0ba544378b806fc9e580e
Stephanie Fu <StephaniefuU@gmail.com>
2017-10-18 08:18:40 +0800

    server set up

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
 const express = require ('express');
+const parser = require('body-parser');
+const morgan = require('morgan');
+
+const PORT = 8080;
+
+const app = express();
+
+app.use(parser.urlencoded({extended:true}));
+app.use(parser.json());
+app.use(morgan('dev'));
+
+app.listen(PORT, err => {
+  if (err) {
+    console.log(`Could not connect to PORT ${PORT}`)
+  } else {
+    console.log(`Successfully connected to PORT ${PORT}`)
+  }
+});
